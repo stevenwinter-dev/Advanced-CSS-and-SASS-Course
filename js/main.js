c8f6b5b0049005b75d84a9da40ceb3d404d369d1
Steven Winter <stevenwinter.me@gmail.com>
2019-03-23 23:59:35 -0500

    More animation work

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementByClassName("animate-left").addEventListener("scroll", myFunction);
+function myFunction() {
+    var element = document.getElementByClassNAme("animate-left");
+    element.classList.add("mystyle");
+  }
